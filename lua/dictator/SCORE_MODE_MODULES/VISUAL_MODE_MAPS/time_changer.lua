7f06a5a5198e3b38780d33c00f17923a7381fb10
Jack Sanders <Guitarbreakerjs@gmail.com>
2022-03-04 02:31:30 -0500

    more notes

--- lua/dictator/SCORE_MODE_MODULES/VISUAL_MODE_MAPS/time_changer.lua
+++ lua/dictator/SCORE_MODE_MODULES/VISUAL_MODE_MAPS/time_changer.lua
@@ -1,9 +1,24 @@
-local promptStart = ":\b\b\b\b\bexec ':lua vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(\":\\<c-u>%s/\\%V/"
+-- CHANGE THE TIME OF NOTES WITH VISUAL BLOCK MODE
+local promptStart = ":\b\b\b\b\bsilent execute ':lua vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(\":\\<c-u>%s/\\%V/"
 local promptEnd = "/g\",true,false,true),\"m\",true)'\r"
+
 local whole_note = ""
+
+local HalfNote = ""
+
+local QuarterNote = ""
+
+local EighthNote = ""
+
+local SixteenthNote = ""
+
 
 local string_table = {
     ['NtoWhole'] = promptStart..whole_note..promptEnd,
+    ['NtoHalf'] = promptStart..HalfNote..promptEnd,
+    ['NtoQuarter'] = promptStart..QuarterNote..promptEnd,
+    ['NtoEighth'] = promptStart..EighthNote..promptEnd,
+    ['NtoSixteenth'] = promptStart..SixteenthNote..promptEnd,
 }
 
 
@@ -11,5 +26,21 @@ return {
     ['W'] = {
         ['rhs'] = string_table['NtoWhole'],
         ['noremap'] = true
-    }
+    },
+    ['H'] = {
+        ['rhs'] = string_table['NtoHalf'],
+        ['noremap'] = true
+    },
+    ['N'] = {
+        ['rhs'] = string_table['NtoQuarter'],
+        ['noremap'] = true
+    },
+    ['E'] = {
+        ['rhs'] = string_table['NtoEighth'],
+        ['noremap'] = true
+    },
+    ['S'] = {
+        ['rhs'] = string_table['NtoSixteenth'],
+        ['noremap'] = true
+    },
 }
